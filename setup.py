9551d47d5291b3f0e10c89ab113f75ececcfb1c3
Nueces <nobody@nueces.org>
2010-06-16 03:50:30 +0000

    remoev plone.app.ergistry dependencie from setup.py to avoid errors on install :s

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(name='yaco.deliverancethemeselector',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          'plone.app.registry',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
